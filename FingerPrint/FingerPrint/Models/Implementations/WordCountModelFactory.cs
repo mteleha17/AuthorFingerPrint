54b61dcf658b49f8972b7c202d0a159f8e11db73
jgray15 <jgray15@jcu.edu>
2016-10-03 15:46:04 -0400

    Added WordCountModelFactory

---
+++ FingerPrint/FingerPrint/Models/Implementations/WordCountModelFactory.cs
@@ -0,0 +1,18 @@
+﻿using FingerPrint.Models.Interfaces.TypeInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FingerPrint.Models.Implementations
+{
+    public class WordCountModelFactory : IWordCountModelFactory<IFlexibleWordCountModel<ISingleWordCountModel>>
+    {
+        public IFlexibleWordCountModel<ISingleWordCountModel> GenerateCounts(TextReader text, IFlexibleWordCountModel<ISingleWordCountModel> model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
