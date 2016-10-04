df39670e3b2c6f0dbccea3d889867e82483581f4
jgray15 <jgray15@jcu.edu>
2016-10-03 21:18:28 -0400

    Added more tests for the WordCountModelFactory.

--- FingerPrint/FingerPrintUnitTests/ModelTests/WordCountModelFactoryTests.cs
+++ FingerPrint/FingerPrintUnitTests/ModelTests/WordCountModelFactoryTests.cs
@@ -11,6 +11,7 @@ namespace FingerPrintUnitTests.ModelTests
     public class WordCountModelFactoryTests
     {
         StringReader stringReader;
+        StreamReader streamReader;
         IFlexibleWordCountModel<ISingleWordCountModel> counts;
         IWordCountModelFactory<IFlexibleWordCountModel<ISingleWordCountModel>> factory;
 
@@ -41,6 +42,61 @@ namespace FingerPrintUnitTests.ModelTests
             Assert.AreEqual(0, counts.GetAt(true, 7));
             Assert.AreEqual(1, counts.GetAt(true, 8));
             Assert.AreEqual(0, counts.GetAt(true, 9));
+        }
+
+        [TestMethod]
+        public void CountWithoutQuotes()
+        {
+            string s = "As Thomas Jefferson once said, \"Hey there buddy.\"";
+            stringReader = new StringReader(s);
+            factory.GenerateCounts(stringReader, counts);
+            Assert.AreEqual(0, counts.GetAt(false, 0));
+            Assert.AreEqual(1, counts.GetAt(false, 1));
+            Assert.AreEqual(0, counts.GetAt(false, 2));
+            Assert.AreEqual(2, counts.GetAt(false, 3));
+            Assert.AreEqual(0, counts.GetAt(false, 4));
+            Assert.AreEqual(1, counts.GetAt(false, 5));
+            Assert.AreEqual(0, counts.GetAt(false, 6));
+            Assert.AreEqual(0, counts.GetAt(false, 7));
+            Assert.AreEqual(1, counts.GetAt(false, 8));
+            Assert.AreEqual(0, counts.GetAt(false, 9));
+        }
+
+        [TestMethod]
+        public void CountWordAcrossLines()
+        {
+            string s = "Let's consider a word spanning multip-\nle lines. Will the program handle it correctly?";
+            stringReader = new StringReader(s);
+            factory.GenerateCounts(stringReader, counts);
+            Assert.AreEqual(1, counts.GetAt(true, 0));
+            Assert.AreEqual(1, counts.GetAt(true, 1));
+            Assert.AreEqual(1, counts.GetAt(true, 2));
+            Assert.AreEqual(2, counts.GetAt(true, 3));
+            Assert.AreEqual(2, counts.GetAt(true, 4));
+            Assert.AreEqual(1, counts.GetAt(true, 5));
+            Assert.AreEqual(1, counts.GetAt(true, 6));
+            Assert.AreEqual(3, counts.GetAt(true, 7));
+            Assert.AreEqual(1, counts.GetAt(true, 8));
+            Assert.AreEqual(0, counts.GetAt(true, 9));
+        }
+
+        [TestMethod]
+        public void CountWordAcrossLinesFromFile()
+        {
+            using (streamReader = new StreamReader("..\\..\\SampleTextFiles\\WordSpanningMultipleLines.txt"))
+            {
+                factory.GenerateCounts(streamReader, counts);
+                Assert.AreEqual(1, counts.GetAt(true, 0));
+                Assert.AreEqual(1, counts.GetAt(true, 1));
+                Assert.AreEqual(1, counts.GetAt(true, 2));
+                Assert.AreEqual(2, counts.GetAt(true, 3));
+                Assert.AreEqual(2, counts.GetAt(true, 4));
+                Assert.AreEqual(1, counts.GetAt(true, 5));
+                Assert.AreEqual(1, counts.GetAt(true, 6));
+                Assert.AreEqual(3, counts.GetAt(true, 7));
+                Assert.AreEqual(1, counts.GetAt(true, 8));
+                Assert.AreEqual(0, counts.GetAt(true, 9));
+            }
         }
 
     }
