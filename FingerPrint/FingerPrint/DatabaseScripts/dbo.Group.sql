313bce23847a73d4a5658123d733e41749f20b58
cfriess17 <cfriess17@CSVM-65>
2016-11-07 16:13:50 -0500

    Database update

--- FingerPrint/FingerPrint/DatabaseScripts/dbo.Group.sql
+++ FingerPrint/FingerPrint/DatabaseScripts/dbo.Group.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Group] (
     [GroupID] INT        IDENTITY (1, 1) NOT NULL,
     [Name]    NCHAR (10) NULL,
-    PRIMARY KEY CLUSTERED ([GroupID] ASC)
+    PRIMARY KEY CLUSTERED ([GroupID] ASC),
+	FOREIGN KEY ([GroupID]) REFERENCES [dbo].[Text_Group]([GroupID])
 );
 
