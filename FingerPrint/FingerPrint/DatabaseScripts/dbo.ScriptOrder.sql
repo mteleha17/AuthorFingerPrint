fc9e3931831f944a81f2e74f8c6a540d2a64c801
cfriess17 <cfriess17@CSVM-65>
2016-11-18 13:45:45 -0500

    Database

---
+++ FingerPrint/FingerPrint/DatabaseScripts/dbo.ScriptOrder.sql
@@ -0,0 +1,8 @@
+﻿USE [FingerprintV2];
+
+SOURCE DatabaseScripts/[Text.sql];
+SOURCE Fingerprint/DatabaseScripts/[Counts.sql];
+SOURCE Fingerprint/DatabaseScripts/[Text_Group.sql];
+SOURCE Fingerprint/DatabaseScripts/[Group.sql];
+SOURCE Fingerprint/DatabaseScripts/[Text_Group.sql];
+ 
---
+++ FingerPrint/FingerPrint/DatabaseScripts/dbo.Script_Order.sql
@@ -0,0 +1,6 @@
+﻿USE [FingerprintV2];
+SOURCE DatabaseScripts/[dbo.Text.sql];
+SOURCE DatabaseScripts/[dbo.Counts.sql];
+SOURCE DatabaseScripts/[dbo.Text_Group.sql];
+SOURCE DatabaseScripts/[dbo.Group.sql];
+SOURCE DatabaseScripts/[dbo.Group_Group.sql];
